ff135600bec07f76892578b8a70024c71f27c575
宁凡 <728062007@qq.com>
2026-05-22 14:29:20 +0800

    修改

--- bdcharge/SmartChargeAdmin/admin_cfg/settings.py
+++ bdcharge/SmartChargeAdmin/admin_cfg/settings.py
@@ -55,9 +55,15 @@ MIDDLEWARE = [
 ]
 
 #跨域增加忽略
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = ( '*' )
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost",
+#     "http://127.0.0.1",
+#     "http://你的服务器IP",
+#     "https://你的域名",
+# ]
+# CORS_ORIGIN_WHITELIST = ( '*' )
 
 CORS_ALLOW_METHODS = (
     'DELETE',
